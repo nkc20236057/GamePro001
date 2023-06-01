dd8b01998f9257a8f5f7d024f1c49cf8325d413e
nkc20236057 <nkc.sugiura.shin@gmail.com>
2023-06-01 09:45:47 +0900

    5/31

--- GamePro/Assets/Script/kmcount.cs
+++ GamePro/Assets/Script/kmcount.cs
@@ -5,12 +5,5 @@ using UnityEngine.UI;
 
 public class kmcount : MonoBehaviour
 {
-    float meter;
-    float meterText;
-
-    void Update()
-    {
-        this.meter += Time.deltaTime * 60;
-        this.meterText.GetComponent<Text>().text = this.meter.ToString("F1") + "km";
-    }
+   
 }
